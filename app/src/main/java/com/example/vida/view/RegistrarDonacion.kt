3b97d451ca40d3e59026a07e8e394ac1c8ae88b9
danielro999 <persensus@yahoo.com.ar>
2024-11-13 18:28:34 -0300

    merge main con rama mapa

--- app/src/main/java/com/example/vida/view/RegistrarDonacion.kt
+++ app/src/main/java/com/example/vida/view/RegistrarDonacion.kt
@@ -7,15 +7,8 @@ import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vida.R
-import com.example.vida.repository.DonacionRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class RegistrarDonacion : AppCompatActivity() {
-
-    private val donacionRepository = DonacionRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
